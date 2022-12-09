968a6dad4cf1e37c6eff4be157313eb84bc5532d
Chayathon <chayathon_p@cmu.ac.th>
2022-12-09 16:07:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,33 @@
 using namespace std;
 
 int main(){
+    int data;
+    int countE=0;
+    int countO=0;
     cout << "Enter an integer: ";
+    cin >> data;
+    
+    while(data != 0)
+    {
+    cout << "Enter an integer: ";
+
+    
+     if(data %2 != 0)
+    {
+    countO++;
+    }
+    else
+    {
+    countE++;
+    }
+cin >> data;
+    }
+    
+
+    
     cout << "#Even numbers = ";
+    cout << countE << "\n";
     cout << "#Odd numbers = ";
+    cout << countO;
     return 0;
 }
